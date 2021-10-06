493f4448120657707018fee1218ff888ff5823f6
범뚱 <79968715+kk7073@users.noreply.github.com>
2021-10-06 11:30:46 +0900

    범뚱

---
+++ app/src/main/java/com/cookandroid/splashscreen/MainActivity.java
@@ -0,0 +1,18 @@
+package com.cookandroid.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //함창범 : 안녕 친구
+        //최범준 : 범뚱
+        // ㅗ
+        // 창범준
+    }
+}

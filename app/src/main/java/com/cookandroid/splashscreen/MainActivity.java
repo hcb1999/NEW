123f0d13f1cec4b103adb5628bc1efaa36a47452
hcb1999 <79966891+hcb1999@users.noreply.github.com>
2021-10-25 19:39:40 +0900

    hcb1999_10_25

--- app/src/main/java/com/cookandroid/splashscreen/MainActivity.java
+++ app/src/main/java/com/cookandroid/splashscreen/MainActivity.java
@@ -2,7 +2,13 @@ package com.cookandroid.splashscreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,9 +16,87 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //함창범 : 안녕 친구
-        //최범준 : 범뚱
-        // ㅗ
-        // 창범준
+
+        final EditText et_time = (EditText) findViewById(R.id.Time); //아이디 이용해서 변수생성
+        final EditText et_time2 = (EditText) findViewById(R.id.Time2);
+        final EditText alarmtime = (EditText) findViewById(R.id.alarmtime);
+
+        et_time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar mcurrentTime = Calendar.getInstance();
+                int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+                int minute = mcurrentTime.get(Calendar.MINUTE);
+                TimePickerDialog mTimePicker;
+
+                mTimePicker = new TimePickerDialog(MainActivity.this,android.R.style.Theme_Holo_Light_Dialog_NoActionBar, new TimePickerDialog.OnTimeSetListener() {
+                    @Override//타임피커 객체생성
+                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                        String state = "AM";
+                        // 선택한 시간이 12를 넘을경우 "PM"으로 변경 및 -12시간하여 출력 (ex : PM 6시 30분)
+                        if (selectedHour > 12) {
+                            selectedHour -= 12;
+                            state = "PM";
+                        }
+                        // EditText에 출력할 형식 지정
+                        et_time.setText(state + " " + selectedHour + "시 " + selectedMinute + "분");
+                    }
+                }, hour, minute, false); // true의 경우 24시간 형식의 TimePicker 출현
+                mTimePicker.setTitle("Select Time");
+                mTimePicker.show();
+            }
+        });
+        et_time2.setOnClickListener(new View.OnClickListener() { //위코드와 동일 아이디만 다름
+            @Override
+            public void onClick(View v) {
+                Calendar mcurrentTime = Calendar.getInstance();
+                int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+                int minute = mcurrentTime.get(Calendar.MINUTE);
+                TimePickerDialog mTimePicker;
+
+                mTimePicker = new TimePickerDialog(MainActivity.this,android.R.style.Theme_Holo_Light_Dialog_NoActionBar, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                        String state = "AM";
+                        // 선택한 시간이 12를 넘을경우 "PM"으로 변경 및 -12시간하여 출력 (ex : PM 6시 30분)
+                        if (selectedHour > 12) {
+                            selectedHour -= 12;
+                            state = "PM";
+                        }
+                        // EditText에 출력할 형식 지정
+                        et_time2.setText(state + " " + selectedHour + "시 " + selectedMinute + "분");
+                    }
+                }, hour, minute, false); // true의 경우 24시간 형식의 TimePicker 출현
+                mTimePicker.setTitle("Select Time");
+                mTimePicker.show();
+            }
+        });
+        alarmtime.setOnClickListener(new View.OnClickListener() {//위코드와 동일 아이디만 다름
+            @Override
+            public void onClick(View v) {
+                Calendar mcurrentTime = Calendar.getInstance();
+                int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+                int minute = mcurrentTime.get(Calendar.MINUTE);
+                TimePickerDialog mTimePicker;
+
+                mTimePicker = new TimePickerDialog(MainActivity.this,android.R.style.Theme_Holo_Light_Dialog_NoActionBar, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                        String state = "AM";
+                        // 선택한 시간이 12를 넘을경우 "PM"으로 변경 및 -12시간하여 출력 (ex : PM 6시 30분)
+                        if (selectedHour > 12) {
+                            selectedHour -= 12;
+                            state = "PM";
+                        }
+                        // EditText에 출력할 형식 지정
+                        alarmtime.setText(state + " " + selectedHour + "시 " + selectedMinute + "분");
+                    }
+                }, hour, minute, false); // true의 경우 24시간 형식의 TimePicker 출현
+                mTimePicker.setTitle("Select Time");
+                mTimePicker.show();
+            }
+        });
+
     }
+
 }
